3e869f8ddd5730a499b7e9b53e85a17a65c0955e
SkylarSL <sliang7113@hwdsb.on.ca>
2018-12-31 01:33:07 -0500

    Update login.js
    now saves user and goes to Sleck website

--- login/login.js
+++ login/login.js
@@ -7,9 +7,20 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+app.use(express.static(__dirname + "/public/login"));
+
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/html/index.html");
+    res.sendFile(__dirname + "/public/login/login.html");
+})
+
+app.post("/addUser", function(req, res){
+    var user = new User(req.body);
+    user.save();
+    console.log("saved: " + user);
+    res.sendFile(__dirname + "/public/html/index.html");
 });
+
+app.use(express.static(__dirname + "/public/html"));
 
 server.listen(port, () => {
     console.log("listening on " + port)
